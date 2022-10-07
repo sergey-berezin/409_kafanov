6b55325ffeb871789cb687b6d9aab37b25ef5587
kafanovn <92315991+kafanovn@users.noreply.github.com>
2022-10-07 23:55:54 +0300

    Add files via upload

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.ML.OnnxRuntime;
+using Microsoft.ML.OnnxRuntime.Tensors;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+
+namespace NuGet_ArcFace_Embedder {
+
+    public class img_embed
+    {
+        private InferenceSession Session;
+        private Dictionary<string, Task<float[]>> CalculationsCollection;
+        SemaphoreSlim semaphore;
+
+        public img_embed()
+        {
+            this.Session = new InferenceSession("arcfaceresnet100-8.onnx");
+            this.semaphore = new SemaphoreSlim(1);
+            this.CalculationsCollection = new Dictionary<string, Task<float[]>>();
+        }
+
+        ~img_embed()
+        {
+            this.Session.Dispose();
+        }
+
+        public float Length(float[] v) => (float)Math.Sqrt(v.Select(x => x * x).Sum());
+
+        public float[] Normalize(float[] v)
+        {
+            var len = Length(v);
+            return v.Select(x => x / len).ToArray();
+        }
+
+        public DenseTensor<float> ImageToTensor(Image<Rgb24> img)
+        {
+            var w = img.Width;
+            var h = img.Height;
+            var t = new DenseTensor<float>(new[] { 1, 3, h, w });
+
+            img.ProcessPixelRows(pa =>
+            {
+                for (int y = 0; y < h; y++)
+                {
+                    Span<Rgb24> pixelSpan = pa.GetRowSpan(y);
+                    for (int x = 0; x < w; x++)
+                    {
+                        t[0, 0, y, x] = pixelSpan[x].R;
+                        t[0, 1, y, x] = pixelSpan[x].G;
+                        t[0, 2, y, x] = pixelSpan[x].B;
+                    }
+                }
+            });
+
+            return t;
+        }
+
+        public float[] GetEmbeddings(Image<Rgb24> face)
+        {
+            string session_key = Guid.NewGuid().ToString();
+            var inputs = new List<NamedOnnxValue> { NamedOnnxValue.CreateFromTensor("data", ImageToTensor(face)) };
+
+            semaphore.WaitAsync();
+            using IDisposableReadOnlyCollection<DisposableNamedOnnxValue> results = Session.Run(inputs);
+            Console.WriteLine("222");
+            semaphore.Release();
+
+            return Normalize(results.First(v => v.Name == "fc1").AsEnumerable<float>().ToArray());
+        }
+    }
+}
---
+++ ClassLibrary1/EmptyClass.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using NuGet_ArcFace_Embedder;
+
+
+namespace NuGet_ArcFace_Functions
+{
+    public class img_comp
+    {
+        public float Length(float[] v) => (float)Math.Sqrt(v.Select(x => x * x).Sum());
+        public float Distance(float[] v1, float[] v2) => Length(v1.Zip(v2).Select(p => p.First - p.Second).ToArray());
+        public float Similarity(float[] v1, float[] v2) => v1.Zip(v2).Select(p => p.First * p.Second).Sum();
+
+        public delegate T CalculationCallback<T>(float[] v1, float[] v2);
+
+        public T Execute<T>(Image<Rgb24> img1, Image<Rgb24> img2, CalculationCallback<T> callback)
+        {
+
+            float[] embeddings1 = embedder.GetEmbeddings(img1);
+            float[] embeddings2 = embedder.GetEmbeddings(img2);
+            return callback(embeddings1, embeddings2);
+        }
+
+
+        public async Task<float> ExecuteAsync(Image<Rgb24> img1, Image<Rgb24> img2, CalculationCallback<float> callback)
+        {
+            Func<float> embeddings =
+                () =>
+                {
+                    float[] embeddings1 = embedder.GetEmbeddings(img1);
+                    float[] embeddings2 = embedder.GetEmbeddings(img2);
+                    return callback(embeddings1, embeddings2);
+                };
+            var res = Task<float>.Run(embeddings);
+            return res.Result;
+        }
+
+        public float Distance(Image<Rgb24> img1, Image<Rgb24> img2)
+        { return Execute<float>(img1, img2, Distance); }
+
+        public float Similarity(Image<Rgb24> img1, Image<Rgb24> img2)
+        { return Execute<float>(img1, img2, Similarity); }
+
+        public (float distance, float similarity) Distance_and_Similarity(Image<Rgb24> img1, Image<Rgb24> img2)
+        { return (Execute<float>(img1, img2, Distance), Execute<float>(img1, img2, Similarity)); }
+
+        public async Task<float> AsyncDistance(Image<Rgb24> img1, Image<Rgb24> img2)
+        {
+            var res = await ExecuteAsync(img1, img2, Distance);
+            return res;
+        }
+
+        public async Task<float> AsyncSimilarity(Image<Rgb24> img1, Image<Rgb24> img2)
+        {
+            var res = await ExecuteAsync(img1, img2, Similarity);
+            return res;
+        }
+
+        public img_embed embedder;
+
+        public img_comp()
+        {
+            this.embedder = new img_embed();
+        }
+
+        public void result(Image<Rgb24> img1, Image<Rgb24> img2)
+        {
+            float[] embeddings1 = embedder.GetEmbeddings(img1);
+            float[] embeddings2 = embedder.GetEmbeddings(img2);
+            Console.WriteLine($"Distance =  {Distance(embeddings1, embeddings2) * Distance(embeddings1, embeddings2)}");
+            Console.WriteLine($"Similarity =  {Similarity(embeddings1, embeddings2)}");
+        }
+    }
+}
