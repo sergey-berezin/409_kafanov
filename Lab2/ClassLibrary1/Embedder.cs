456c7a1b37a07fded007e53e29c6afc9961bf139
kafanovn <92315991+kafanovn@users.noreply.github.com>
2022-10-30 17:58:16 +0300

    Add files via upload

---
+++ Lab2/ClassLibrary1/Embedder.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using NuGet_ArcFace_Embedder;
+
+
+namespace NuGet_ArcFace_Functions
+{
+    public class img_comp
+    {
+        public float Length(float[] v) => (float)Math.Sqrt(v.Select(x => x * x).Sum());
+        public float Distance(float[] v1, float[] v2) => Length(v1.Zip(v2).Select(p => p.First - p.Second).ToArray());
+        public float Similarity(float[] v1, float[] v2) => v1.Zip(v2).Select(p => p.First * p.Second).Sum();
+
+        public delegate T CalculationCallback<T>(float[] v1, float[] v2);
+
+        public T Execute<T>(Image<Rgb24> img1, Image<Rgb24> img2, CalculationCallback<T> callback)
+        {
+
+            float[] embeddings1 = embedder.GetEmbeddings(img1);
+            float[] embeddings2 = embedder.GetEmbeddings(img2);
+            return callback(embeddings1, embeddings2);
+
+        }
+
+        public async Task<float> ExecuteAsync(Image<Rgb24> img1, Image<Rgb24> img2, CalculationCallback<float> callback, CancellationToken token)
+        {
+            var res = await Task<float>.Run(async () =>
+            {
+                float[] embeddings1 = await embedder.GetEmbeddingsAsync(img1, token);
+                float[] embeddings2 = await embedder.GetEmbeddingsAsync(img2, token);
+                Console.WriteLine($"Distance =  {Distance(embeddings1, embeddings2) * Distance(embeddings1, embeddings2)}");
+                return callback(embeddings1, embeddings2);
+            });
+            return res;
+
+        }
+
+        public float Distance(Image<Rgb24> img1, Image<Rgb24> img2)
+        { return Execute<float>(img1, img2, Distance); }
+
+        public float Similarity(Image<Rgb24> img1, Image<Rgb24> img2)
+        { return Execute<float>(img1, img2, Similarity); }
+
+        public (float distance, float similarity) Distance_and_Similarity(Image<Rgb24> img1, Image<Rgb24> img2)
+        { return (Execute<float>(img1, img2, Distance), Execute<float>(img1, img2, Similarity)); }
+
+        public Task<float> AsyncDistance(Image<Rgb24> img1, Image<Rgb24> img2, CancellationToken token)
+        {
+            var res = ExecuteAsync(img1, img2, Distance, token);
+            return res;
+        }
+
+        public async Task<float> AsyncSimilarity(Image<Rgb24> img1, Image<Rgb24> img2, CancellationToken token)
+        {
+            var res = await ExecuteAsync(img1, img2, Similarity, token);
+            return res;
+        }
+
+        public img_embed embedder;
+
+        public img_comp()
+        {
+            this.embedder = new img_embed();
+        }
+    }
+}
---
+++ Lab2/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using NuGet_ArcFace_Functions;      //подключили библиотеку классов из нашего пакета
+using System.Threading;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        private List<string> list_images_path;
+        private List<Image<Rgb24>> list_images;
+        private CancellationTokenSource cancelTokenSource;
+        private CancellationToken token;
+        private bool calculations_status;
+        img_comp obj1;
+        public MainWindow()
+        {
+            InitializeComponent();
+            cancelTokenSource = new CancellationTokenSource();
+            token = cancelTokenSource.Token;
+            list_images_path = new List<string>();
+            list_images = new List<Image<Rgb24>>();
+            obj1 = new img_comp();
+            calculations_status = false;
+        }
+
+        //Метод загружает изображения с выбранного каталога и вызввает метод, который строит сетку
+        private void Button_Open_Images(object sender, RoutedEventArgs e)
+        {
+            Gride_Clear();
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            ofd.Multiselect = true;
+            ofd.Filter = "Images (*.jpg, *.png)|*.jpg;*.png";
+            var projectRootFolder = System.IO.Path.GetFullPath("../../../../Images");
+            ofd.InitialDirectory = projectRootFolder;
+            var response = ofd.ShowDialog();
+            if (response == true)
+            {
+                foreach (var path in ofd.FileNames)
+                {
+                    var face = SixLabors.ImageSharp.Image.Load<Rgb24>(path);
+                    list_images.Add(face);
+                    list_images_path.Add(path);
+                }
+            }
+            Grid_Construct();
+        }
+
+        //Метод строит сетку по каталогу изображений
+        private void Grid_Construct()
+        {
+            int n = list_images_path.Count;
+            for (int i = 0; i < n + 1; i++)
+            {
+                table.RowDefinitions.Add(new RowDefinition() { Height = new GridLength(1, GridUnitType.Star) });
+                table.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1, GridUnitType.Star) });
+
+                if (i > 0)
+                {
+                    var uri = new System.Uri(list_images_path[i - 1]);
+                    var bitmap = new BitmapImage(uri);
+
+                    var image1 = new System.Windows.Controls.Image();
+                    image1.Source = bitmap;
+                    var image2 = new System.Windows.Controls.Image();
+                    image2.Source = bitmap;
+
+                    Grid.SetColumn(image1, 0);
+                    Grid.SetRow(image1, i);
+                    table.Children.Add(image1);
+
+                    Grid.SetColumn(image2, i);
+                    Grid.SetRow(image2, 0);
+                    table.Children.Add(image2);
+                }
+            }
+        }
+
+        //метод очищает сетку, массивы с изображениями, обновляет токены
+        public void Gride_Clear()
+        {
+            calculations_status = false;
+            cancelTokenSource = new CancellationTokenSource();
+            token = cancelTokenSource.Token;
+            int size = list_images.Count;
+            if (size == 0)
+                return;
+            table.Children.Clear();
+            pbStatus.Value = 0;
+            for (int i = 0; i < size + 1; i++)
+            {
+                table.RowDefinitions.Clear();
+            }
+            for (int i = 0; i < size + 1; i++)
+            {
+                table.ColumnDefinitions.Clear();
+            }
+            list_images_path.Clear();
+            list_images.Clear();
+        }
+
+        //Метод начинает вычисления по заданным изображениям
+        private async void Button_Start_Calculations(object sender, RoutedEventArgs e)
+        {
+            if (list_images.Count == 0)
+            {
+                MessageBox.Show("Пожалуйста, выберите каталог с изображениями.");
+                return;
+            }
+            if (calculations_status)
+            {
+                MessageBox.Show("Вычисления уже произведены. Пожлауйста, обновите матрицу.");
+                return;
+            }
+            int step1 = 250 / list_images.Count;
+            int step2 = 500 / (list_images.Count * list_images.Count);
+            var tasks = new List<Task>();
+            /*
+            for (int i = 0; i < list_images.Count; i++)
+            {
+                try
+                {
+                    Task task1 = obj1.AsyncDistance(list_images[i], token);
+                    pbStatus.Value += step1;
+                    tasks.Add(task1);
+                }
+                catch (OperationCanceledException e1)
+                {
+                    Console.WriteLine($"{nameof(OperationCanceledException)} thrown with message: {e1.Message}");
+                }
+            }
+
+            try
+            {
+                await Task.WhenAll(tasks);
+            }
+            catch (OperationCanceledException e2)
+            {
+                Console.WriteLine($"{nameof(OperationCanceledException)} thrown with message: {e2.Message}");
+            }
+            */
+
+            pbStatus.Value += step1 * list_images.Count;
+
+            for (int i = 0; i < list_images.Count; i++)
+            {
+                for (int j = 0; j < list_images.Count; j++)
+                {
+                    var l = new Label();
+                    Grid.SetColumn(l, i + 1);
+                    Grid.SetRow(l, j + 1);
+                    l.HorizontalAlignment = HorizontalAlignment.Center;
+                    l.VerticalAlignment = VerticalAlignment.Center;
+                    l.FontSize = 12;
+                    if (token.IsCancellationRequested)
+                    {
+                        l.Content = $"Distance: Not calculated\n Similarity: Not calculated";
+                    }
+                    else
+                    {
+                        var res1 = obj1.AsyncDistance(list_images[i], list_images[j], token);
+                        await res1;
+                        l.Content = $"Distance: {res1.Result}\n ";
+                        pbStatus.Value += step2;
+                    }
+                    table.Children.Add(l);
+                }
+                //if(i == 3)
+                //    cancelTokenSource.Cancel();
+            }
+            if (!token.IsCancellationRequested)
+            {
+                pbStatus.Value = 1000;
+            }
+            calculations_status = true;
+        }
+
+        private void Button_Grid_Clear(object sender, RoutedEventArgs e)
+        {
+            if (list_images.Count == 0)
+            {
+                MessageBox.Show("Матрица уже очищена.");
+                return;
+            }
+            Gride_Clear();
+        }
+
+        //Метод отменяет вычисления
+        private void Button_Cancel_Calculations(object sender, RoutedEventArgs e)
+        {
+            cancelTokenSource.Cancel();
+            MessageBox.Show("Вычисления прерваны.");
+        }
+    }
+}
